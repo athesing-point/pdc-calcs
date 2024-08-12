ee39c45c293872f742b42f303790b5037f853034
Austin Thesing <athesing@point.com>
2024-08-12 15:38:34 -0600

    home improvement calc - dev setup

---
+++ home-improvement/calc.js
@@ -0,0 +1,8 @@
+let originationFee = 0.04; // 4% origination fee
+let interestRate = 9.75 / 100; // 9.75% starting interest rate
+var loanAmount = 50000; // $50,000 loan amount
+var loanTerm;
+
+getelementById("loanAmount").value = loanAmount;
+getelementById("interestRate").value = interestRate;
+getelementById("originationFee").value = originationFee;
